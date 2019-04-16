ba47b2b72bfe4cc0f1629e6e82539bef07621585
jimmy <celenh@yahoo.com>
2019-04-17 02:16:39 +0300

    code refactoring + new features

--- functions.js
+++ functions.js
@@ -1,16 +1,54 @@
+// let test = function(value){
+//     return value+1;
+// }
+
+// let test2 = function(value){
+//     alert(value)
+// }
+
+// await only suspends when the value passed to it is a Promise
+// async function f() {
+//     // https://javascript.info/async-await
+//     // The word “async” before a function means one simple thing: a function always returns a promise. Even If a function actually returns a non-promise value, prepending the function definition with the “async” keyword directs Javascript to automatically wrap that value in a resolved promise.
+//     // So, async ensures that the function returns a promise, and wraps non-promises in it. Simple enough, right? But not only that. There’s another keyword, await, that works only inside async functions
+//     // The async keyword before a function has two effects:
+//     // -Makes it always return a promise.
+//     // -Allows to use await in it.
+//     // The await keyword before a promise makes JavaScript wait until that promise settles, and then:
+//     // If it’s an error, the exception is generated, same as if throw error were called at that very place.
+//     // Otherwise, it returns the result, so we can assign it to a value.
+
+//     let res = Promise.resolve(1);
+//     return await res;
+// }
+
+// f().then(test).then(test2);
+
+
 // binding dom
-let input = document.querySelector("input");
-let pre = document.querySelector("#pre");
-let span = document.querySelector("#mid");
-let post = document.querySelector("#post");
+let fileSelector = document.querySelector(".file-input");
+let radio = document.querySelector(".control");
+// let pre = document.querySelector("#pre");
+// let span = document.querySelector("#mid");
+// let post = document.querySelector("#post");
+let mid = '';
 let btn = document.querySelector("button");
 let form = document.querySelector("form");
 let prog = document.querySelector("progress");
 let notification = document.querySelector(".notification");
+let selectedFilesText = document.querySelector('.file-name');
+let codeText = document.querySelector('.code');
+let copyBtn = document.querySelector('#copy');
+let i = null;
 
 let fileList = [];
-let url;
+let entries = [];
+// let url = null;
+// let code = null;
 let dim; // image dimensions old and new
+let prePublii = '<div class="gallery" contenteditable="false" \
+data-is-empty="false" data-columns="3">';
+let postPublii = '</div>';
 
 let dummy = [
     "https://i.imgur.com/pydVVTv.jpg",
@@ -22,17 +60,20 @@ let dummy = [
     "https://i.imgur.com/JGi1Kjg.jpg",
     "https://i.imgur.com/ib5ooka.jpg"
 ];
-// var $files = 0;
 
 let getMaxAllowedImageSize = function () {
     return $('input[type=file]').data("max-size") * 1024;
 }
-
 var maxSize = getMaxAllowedImageSize();
+
+let getSelectedFormat = function () {
+    return document.querySelector('input[name="format"]:checked').value;
+}
+
 
 //this is called multiple times
 //it takes time to upload the img (async)
-let upload = function (file) {
+let upload = async function (file) {
     if (file.length) {
         console.log("Please Select files first");
         return false;
@@ -46,7 +87,7 @@ let upload = function (file) {
     // Begin file upload
     console.log("Uploading file to Imgur...");
 
-    // Replace ctrlq with your own API key
+    // Replace with your own API key
     var apiUrl = 'https://api.imgur.com/3/image';
     var apiKey = '28aaa2e823b03b1';
 
@@ -70,69 +111,84 @@ let upload = function (file) {
 
     // Response contains stringified JSON
     // Image URL available at response.data.link
-    // console.log(0);
-    $.ajax(settings).done(function (response) {
-        let result = JSON.parse(response);
-        url = result.data.link;
+    return new Promise((resolve, reject) => {
+        $.ajax(settings).done(function (response) {
+            let result = JSON.parse(response);
+            entries.push(result);
+            url = result.data.link;
+            console.debug("Resolve upload")
+            progressAddOne();
+            resolve(url);
+        });
+    })
+}
 
-        formatPublii();
-
-        // redirect to the result
-        // window.location.href = result.data.link;
-        // append(result.data.link);
-        // span.innerText = result.data.link
-
-    });
+let imageProcess = async function (url) {
+    return new Promise((resolve, reject) => {
+        dim = {
+            old: {},
+            new: {}
+        }
+        let img = new Image();
+        let fixedWidth = 768; // Mansory Layout
+        img.src = url;
+        img.onload = () => {
+            dim.old.width = img.width;
+            dim.old.height = img.height;
+            let ratio = img.height / img.width;
+            dim.new.width = fixedWidth
+            dim.new.height = Math.floor(ratio * dim.new.width)
+            console.debug("resolve imageProcess");
+            resolve();
+        }
+    })
 }
 
 //it takes time to load the img (async)
-let formatPublii = function () {
-    let img = new Image();
-    dim = {
-        old: {},
-        new: {}
-    }
-    let fixedWidth = 768; // Mansory Layout
-    img.src = url;
+let formatPublii = async function (url) {
 
-    //get image dimensions
-    img.onload = function () { // takes time waits for the image to load
-        dim.old.width = img.width;
-        dim.old.height = img.height;
-        let ratio = img.height / img.width;
-        dim.new.width = fixedWidth
-        dim.new.height = Math.floor(ratio * dim.new.width)
-
-        let code = '<figure class="gallery__item"><a href="' + url + '" data-size="' + dim
+    await imageProcess(url);
+    return new Promise((resolve, reject) => {
+        code = '<figure class="gallery__item"><a href="' + url + '" data-size="' + dim
             .old.width +
             "x" + dim.old.height + '"><img src="' + url + '" alt="" width="' + dim.new
             .width +
             '" height="' + dim.new.height + '"></a></figure>';
-        // console.debug(dim); 
-        
-        append(code);        
-    }
+        console.debug("resolve format")
+        resolve(code);
+    });
+}
 
-    
-    
+let formatMarkdown = async function (url) {
+    // console.log("formatMarkdown");
+    return new Promise((resolve, reject) => {
+        resolve('![Imgur](' + url + ')');
+    })
+}
+
+let appendPublii = function (code) {
+    console.debug("Appending...")
+    mid += code + '\n';
+    codeText.value = prePublii + mid + postPublii;
 
 }
 
-let append = function (inp) {
-    let span = document.querySelector("#mid");
-    let p = document.createElement("p");
-    let text = document.createTextNode(inp);
-    span.appendChild(p);
-    p.appendChild(text);
-    progressAddOne();
-    // console.log(2);
+let append = function (code) {
+    codeText.value += code + ' \n ';
+    // let span = document.querySelector("#mid");
+    // let p = document.createElement("p");
+    // let text = document.createTextNode(code);
+    // span.appendChild(p);
+    // p.appendChild(text);
+    //FIXME: why do all the uploads have to end for the appends to start?
 }
 
-let prepareCode = function () {
-    pre.innerText =
-        '<div class="gallery" contenteditable="false" data-is-empty="false" data-columns="3">';
-    post.innerText = '</div>'
-}
+// let prepareCodePublii = function () {
+//     pre.innerText =
+//         '<div class="gallery" contenteditable="false" \
+//         data-is-empty="false" data-columns="3">';
+//     post.innerText = '</div>';
+// }
 
 let progressAddOne = function () {
     let val = parseInt(prog.getAttribute("value"));
@@ -144,20 +200,60 @@ let progressAddOne = function () {
     }
 }
 
-let refresh = function () {
+let refreshUI = function () {
     if (notification.classList.contains('is-success')) {
         notification.classList.remove('is-success');
         notification.classList.add('is-warning');
     }
-    if (btn.getAttribute('disabled') != null) {
+    if (fileList.length > 0) {
         btn.removeAttribute('disabled');
+        prog.setAttribute("max", fileList.length);
     }
-    prog.setAttribute("max", fileList.length);
     prog.setAttribute("value", "0");
+}
+
+let createFileList = function () {
+    fileList = [];
+    // Notice how the fileList array is reset inside the change handler. 
+    // This is in case the user selects files more than once.
+    // We don't want to upload the same image multiple times
+    for (let i = 0; i < fileSelector.files.length; i++) {
+        fileList.push(fileSelector.files[i]);
+    }
+}
+
+let showFileSelectorText = function () {
+    if (fileSelector.files.length > 0) {
+        let f = (fileSelector.files.length > 1) ? ' Files' : ' File'; // Plural
+        selectedFilesText.innerHTML = fileSelector.files.length + f + ' Selected';
+    }
 }
 
 let lock = function () {
     notification.classList.remove('is-warning');
     notification.classList.add('is-success');
     btn.setAttribute('disabled', '');
+}
+
+let newCode = function (option) {
+    codeText.value = '';
+    refreshUI();
+    mid = ''; // for Publii
+    if (entries.length <= 0) {
+        return;
+    }
+    if (option == 'publii') {
+
+        entries.forEach(function (entry) {
+            formatPublii(entry.data.link).then(appendPublii);
+        });
+    } else if (option == "markdown") {
+        entries.forEach(function (entry) {
+            formatMarkdown(entry.data.link).then(append);
+        });
+    } else {
+        entries.forEach(function (entry) {
+            append(entry.data.link);
+        });
+    }
 }
